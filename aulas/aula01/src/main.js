1cd2c698023e18b06843841ebc5835d7be616ef0
Brandon <brenorochagr@gmail.com>
2026-03-17 11:00:39 -0300

    adicionando codigo javascript

--- aulas/aula01/src/main.js
+++ aulas/aula01/src/main.js
@@ -1,1 +1,38 @@
 import './style.css'
+/*
+validar o formulario com as regras:
+- matricula > 0 e obrigatoria
+- senha tamanhp > 6 caracteres
+*/
+
+const matricula = document.querySelector('#Matricula');
+const senha = document.querySelector('#Senha');
+const botao = document.querySelector('button');
+
+botao.addEventListener('click', function(e) {
+  const matriculaErro = document.querySelector('#matriculaErro');
+  const senhaErro = document.querySelector('#senhaErro');
+
+  matriculaErro.textContent = '';
+  senhaErro.textContent = '';
+
+  if (matricula.value == '') {
+    console.log('Matricula obrigatoria');
+    return;
+  }
+
+  if (parseInt(matricula.value) <= 0) {
+    console.log('Matricula deve ser um numero positivo')
+  }
+
+  if (senha.value == '') {
+    console.log('Senha e obrigatoria');
+    return;
+  }
+
+  if (senha.value.length < 8) {
+    console.log('Senha deve ter no minimo 8 caracteres');
+    return;
+  }
+
+});
