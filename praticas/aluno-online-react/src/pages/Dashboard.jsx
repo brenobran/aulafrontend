c89fb8ef54e54e35d5aee98a16d944ed0d79e8b1
Brandon <brenorochagr@gmail.com>
2026-04-13 16:00:16 -0300

    feat:conclui pratica03.Fecha #7

---
+++ praticas/aluno-online-react/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Main from "../componets/Main";
+import Sidebar from "../componets/Sidebar";
+
+function Dashboard() {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+}
+
+export default Dashboard;
