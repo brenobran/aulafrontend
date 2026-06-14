e3d9708a11d74fcf5da7ada00a03d62ce77a83bf
Brandon <brenorochagr@gmail.com>
2026-06-14 20:59:54 -0300

    Desc

--- praticas/aluno-online-react/src/componets/Main.jsx
+++ praticas/aluno-online-react/src/componets/Main.jsx
@@ -1,17 +1,16 @@
 import Card from "./Card";
 import Topbar from "./Topbar";
 
-function Main() {
+function Main(props) {
   return (
     <>
-      <main className="flex-1 mx-auto pr-6 pl-6">
-        <Topbar />
+      <main className="flex-1">
+        <Topbar titulo={props.titulo} />
 
-        <h2 className="text-lg font-semibold px-4 md:px-10 p-8">
-          Bem - vindo ao Portal do Aluno
+        <h2 className="text-lg font-semibold px-8 md:px-10 p-8">
+          {props.subtitulo}
         </h2>
-
-        <Card />
+        <section className="grid grid-cols-1 md:grid-cols-1 gap-4 p-8 ">{props.children}</section>
       </main>
     </>
   );
